f61d3fd0ae570e66a9adfd483287dc41756d96d6
Rodrivdlc <rodri.vdlc@gmail.com>
2024-10-27 22:09:28 +0100

    cfg

--- app/src/main/java/com/example/novelas/Novela.kt
+++ app/src/main/java/com/example/novelas/Novela.kt
@@ -1,10 +1,13 @@
 package com.example.novelas
 
 data class Novela(
-    val titulo: String,
-    val autor: String,
-    val anoPublicacion: Int,
-    val sinopsis: String,
+    val titulo: String = "",
+    val autor: String = "",
+    val anoPublicacion: Int = 0,
+    val sinopsis: String = "",
     var esFavorita: Boolean = false,
     val reseñas: MutableList<String> = mutableListOf()
-)
+) {
+    // No-argument constructor for Firebase
+    constructor() : this("", "", 0, "", false, mutableListOf())
+}
